843cbde39c792089e9b53083cc5055fabec5640d
bolbass <ann.bolbass@gmail.com>
2022-02-19 15:38:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length <= 1)
+  return false
+let m, ch
+let arr = []
+let ob = ['[', '{', '(']
+let cb = [']', '}', ')']
+for (let i = 0; i < str.length; i++) {
+  ch = str[i]
+  if (cb.indexOf(ch) > -1) {
+    m = ob[cb.indexOf(ch)]
+    if (arr.length == 0 || (arr.pop() != m)) {
+      return false
+    }
+  } else {
+    arr.push(ch)
+  }
+}
+return (arr.length == 0)
 }
